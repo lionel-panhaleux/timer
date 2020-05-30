1d992168168f9c47f965d764d53da7b0a884726f
Lionel Panhaleux <lionel.panhaleux@gmail.com>
2020-05-30 10:42:43 +0200

    New timer
    A more proper use of asyncio, avoids refresh/resume bugs.

--- src/timer_bot.py
+++ src/timer_bot.py
@@ -19,8 +19,8 @@ async def on_ready():
 #: regex use to parse time expression for timer initialisation
 REGEX = (
     r"\s*(?P<high>\d{1,2})"
-    r"\s*((?P<fraction>\.\d+)|:(?P<low>\d{1,2}))?"
-    r"\s*(?P<unit>h|hour|mn|min|minute|s|second)?s?\s*"
+    r"\s*((?P<fraction>\.\d+)|(?P<separator>:|')(?P<low>\d{1,2}))?"
+    r"\s*(?P<unit>h|hour|mn|min|minute|s|second)?s?\s*'*\s*"
 )
 
 #: multipliers for accepted time units
@@ -62,217 +62,121 @@ DISPLAY_SECONDS = 5 * 60
 
 
 class Timer:
-    """The timer. One active timer per channel."""
-
-    def __init__(self, start_time, total_time, channel):
-        """Initialization
-
-        Attributes:
-            start_time (int): start time in seconds, changes to adjust for pauses
-            total_time (int): time until completion, never changes
-            time_left (int): time left, adjusted by self.countdown()
-            paused (int): If set, timer was paused at this time and waits for resume
-            channel (discord.Channel): the channel this timer runs into
-            message (discord.Message): the active timer embed message
-            task_countdown (asyncio.Task):
-                updates message embed and sends thresholds notifications
-            task_reaction (asyncio.Task): waits for ⏱ or 🛑 to pause or stop
-            unpause (asyncio.Future): when paused, waits for ⏱ removal to resume
-            thresholds (list): list of times on which a message is sent for notification
-        """
-        self.start_time = start_time
-        self.total_time = total_time
-        self.time_left = total_time
-        self.paused = None
+    def __init__(self, channel, start_time, time, log_prefix=""):
         self.channel = channel
-        self.message = None
-        self.task_countdown = None
-        self.task_reaction = None
-        self.unpause = None
+        self.start_time = start_time
+        self.total_time = time
+        self.time_left = time
+        self.log_prefix = log_prefix + "|internal"
         self.thresholds = []
         for limit in THRESHOLDS:
-            if total_time > limit:
+            if time > limit:
                 self.thresholds.append(limit)
 
-    async def run(self):
-        """Run timer
-        """
-        client.TIMERS[self.channel] = self
-        # a pause will wait for resume then start the loop anew
-        while True:
-            self.task_countdown = asyncio.create_task(self.countdown())
-            self.task_reaction = asyncio.create_task(
+        self.message = None
+        self.paused = None
+        self.reaction_future = None
+        self.unpause_future = None
+
+    async def wait_reaction(self):
+        while self.time_left > 0:
+            if not self.message:
+                self.message = await self.channel.send(embed=self.embed())
+                try:
+                    for reaction in ["⏱", "🛑"]:
+                        await self.message.add_reaction(reaction)
+                except discord.Forbidden:
+                    logging.warning(f"[{self.log_prefix}] Missing reaction permission")
+            self.reaction_future = asyncio.ensure_future(
                 client.wait_for(
                     "reaction_add",
                     # avoid timeouting before countdown finishes
                     timeout=self.time_left + 60,
                     # beware not to trigger on our own reaction, they are added async
                     check=lambda reaction, user: (
-                        str(reaction.emoji) in ["⏱", "🛑"] and user != client.user
+                        reaction.message.id == self.message.id
+                        and str(reaction.emoji) in ["⏱", "🛑"]
+                        and user != client.user
                     ),
                 )
             )
-
-            done, pending = await asyncio.wait(
-                {self.task_countdown, self.task_reaction},
-                return_when=asyncio.FIRST_COMPLETED,
-            )
-            for task in done:
-                # cancelled because of a refresh
-                # reaction timeout after countdown finished
-                if task.exception():
-                    continue
-                # "normal" finish
-                if task == self.task_countdown:
-                    logger.info(f"Countdown finished")
-                    self.task_countdown = None
-                    await self.stop()
-                    break
-                if task == self.task_reaction:
-                    self.task_reaction = None
-                    reaction, user = task.result()
-                    # manual stop
-                    if reaction.emoji == "🛑":
-                        logger.info(f"[{user.name}] reaction stop")
-                        await self.stop()
-                        break
-                    # pause
-                    if reaction.emoji == "⏱":
-                        logger.info(f"[{user.name}] reaction pause")
-                        try:
-                            # removes the 🛑 reaction, cancels countdown, updates embed
-                            await self.pause(user)
-                        # a pause over 30 minutes is considered a stop
-                        except asyncio.TimeoutError:
-                            logger.info(f"[{user.name}] lingering pause, stopping")
-                            await self.stop()
-                            break
-                        except asyncio.CancelledError:
-                            # if cancelled it means
-                            # stop() or refresh() have been called: do not stop.
-                            break
-                        self.resume()
-                        # restore reaction and embed, loop to recreate tasks
-                        await self.message.edit(embed=self.embed())
-                        await self.message.add_reaction("🛑")
-            # continue if we're resuming from pause
-            else:
+            try:
+                reaction, user = await self.reaction_future
+            except asyncio.CancelledError:  # refresh
+                logging.info(f"[{self.log_prefix}] Reaction cancelled")
                 continue
-            # all other exits use a break and return, this finishes the run
-            return
+            self.reaction_future = None
+            if reaction.emoji == "🛑":
+                logger.info(f"[{self.log_prefix}] ({user.name}) reaction stop")
+                await self.stop()
+                return
+            if reaction.emoji == "⏱":
+                logger.info(f"[{self.log_prefix}] ({user.name}) reaction pause")
+                try:
+                    await self.pause(user)
+                except asyncio.CancelledError:  # refresh
+                    logging.info(f"[{self.log_prefix}] Pause cancelled")
+                    continue
+
+    async def run(self):
+        client.TIMERS[self.channel] = self
+        self.run_future = asyncio.gather(self.countdown(), self.wait_reaction())
+        try:
+            await self.run_future
+        except asyncio.CancelledError:
+            logger.info(f"[{self.log_prefix}] Cancelled")
+        except asyncio.TimeoutError:
+            logger.info(f"[{self.log_prefix}] Timeout")
+        finally:
+            del client.TIMERS[self.channel]
 
     async def stop(self):
-        """Terminate the timer"""
-        if self.channel in client.TIMERS and client.TIMERS[self.channel] is self:
-            del client.TIMERS[self.channel]
-        await self._cancel_message()
-        # send a notification
-        # indicate remaining time so the user can setup a new timer if it was a mistake
         if self.time_left > 0:
             await self.channel.send("Stopped with " + self.time_str())
         else:
             await self.channel.send("Finished")
-
-    async def refresh(self):
-        """Deactivate the current embed message and send a new one.
-
-        This is useful if a discussion happens in the channel
-        and the embed is too high in the log.
-        """
-        await self._cancel_message()
-        self.message = await self.channel.send(embed=self.embed())
-        # If timer is ended, do not add reactions nor run
-        if self.time_left < 1:
-            return
-        try:
-            for reaction in ["⏱", "🛑"]:
-                await self.message.add_reaction(reaction)
-        except discord.Forbidden:
-            logging.warning("Missing reaction permission")
-        await self.run()
-
-    async def _cancel_message(self):
-        """Delete the current embed, resume the timer if it was paused
-
-        This cancels all running tasks and delete the message.
-        Note the timer needs to be resumed because it makes no sense to cancel
-        the unpause task (waiting for the user to remove his ⏱ reaction)
-        and keep a self.paused value: if refresh() is used, the user wont be able
-        to unpause without pausing first...
-        """
-        if self.task_countdown:
-            self.task_countdown.cancel()
-            self.task_countdown = None
-        if self.task_reaction:
-            self.task_reaction.cancel()
-            self.task_reaction = None
-        if self.unpause:
-            self.resume()
-            self.unpause.cancel()
-            self.unpause = None
+        self.time_left = 0
+        logger.info(f"[{self.log_prefix}] Timer stopped")
+        self.run_future.cancel()
         if self.message:
             await self.message.delete()
             self.message = None
 
-    async def countdown(self):
-        """Countdown: update embed, send notifications
-        """
-        while self.time_left > 0:
-            # update frequency depends on time left
-            if self.time_left < DISPLAY_SECONDS:
-                # the is the absolute minimum because of Discord rate limitation
-                await asyncio.sleep(1)
-            else:
-                await asyncio.sleep(30)
-            self.time_left = max(
-                0, self.total_time - max(0, client.loop.time() - self.start_time),
-            )
-            # update the embed, send a notification if we have hit a threshold
-            await self.message.edit(embed=self.embed())
-            if self.thresholds and self.thresholds[-1] >= self.time_left:
-                await self.channel.send(self._time_str(self.thresholds.pop()))
-
     async def pause(self, user):
-        """Pause the timer, wait for ⏱ to resume
-
-        Note that during the pause, task_reaction is not running.
-        Nobody can use a reaction but the user who paused the timer, to resume it.
-
-        This removes the 🛑 reaction as it woud have no effect
-        """
         self.paused = client.loop.time()
-        if self.task_countdown:
-            self.task_countdown.cancel()
-        await self.message.edit(embed=self.embed())
-        await self.message.remove_reaction("🛑", client.user)
-        self.unpause = client.loop.create_future()
-        # dicord.Client.wait_for() code
-        # cannot use the method directly as we want the Future in self.unpause
-        # in order to be able to cancel it if a refresh is done
-        try:
-            listeners = client._listeners["reaction_remove"]
-        except KeyError:
-            listeners = []
-            client._listeners["reaction_remove"] = listeners
-        listeners.append(
-            (
-                self.unpause,
-                lambda reaction, ruser: (str(reaction.emoji) == "⏱" and ruser == user),
+        if self.message:
+            await self.message.edit(embed=self.embed())
+            await self.message.remove_reaction("🛑", client.user)
+        self.unpause_future = asyncio.ensure_future(
+            client.wait_for(
+                "reaction_remove",
+                timeout=1800,
+                # beware not to trigger on our own reaction, they are added async
+                check=lambda reaction, ruser: (
+                    reaction.message.id == self.message.id
+                    and str(reaction.emoji) == "⏱"
+                    and ruser == user
+                ),
             )
         )
-        await asyncio.wait_for(self.unpause, 1800)
-        logger.info(f"[{user.name}] reaction unpause")
+        try:
+            await self.unpause_future
+            self.unpause_future = None
+            if self.message:
+                await self.message.edit(embed=self.embed())
+                await self.message.add_reaction("🛑")
+        finally:
+            self.start_time += client.loop.time() - self.paused
+            self.paused = None
 
-    def resume(self):
-        """Adjust start_time and remove self.paused
-
-        Further actions depend if it was:
-        - a reaction removal to resume (we keep the same embed)
-        - a refresh from `timer resume` (new embed)
-        - a stop from `timer stop` (do nothing)
-        """
-        self.start_time += client.loop.time() - (self.paused or 0)
-        self.paused = None
+    async def refresh(self):
+        if self.message:
+            await self.message.delete()
+            self.message = None
+        if self.reaction_future:
+            self.reaction_future.cancel()
+        if self.unpause_future:
+            self.unpause_future.cancel()
 
     def embed(self):
         """The running timer embed"""
@@ -287,6 +191,28 @@ class Timer:
         title += self.time_str()
         return discord.Embed.from_dict({"title": title, "description": description})
 
+    async def countdown(self):
+        """Countdown: update embed, send notifications
+        """
+        while self.time_left > 0:
+            # update frequency depends on time left
+            if self.time_left < DISPLAY_SECONDS:
+                # the is the absolute minimum because of Discord rate limitation
+                await asyncio.sleep(1)
+            else:
+                await asyncio.sleep(30)
+            if self.paused:
+                continue
+            self.time_left = max(
+                0, self.total_time - max(0, client.loop.time() - self.start_time),
+            )
+            # update the embed, send a notification if we have hit a threshold
+            if self.message:
+                await self.message.edit(embed=self.embed())
+            if self.thresholds and self.thresholds[-1] >= self.time_left:
+                await self.channel.send(self._time_str(self.thresholds.pop()))
+        await self.stop()
+
     def time_str(self):
         return self._time_str(self.time_left)
 
@@ -294,11 +220,11 @@ class Timer:
     def _time_str(time):
         """Returns a human readable string for given time (int) in seconds"""
         if time > 3600:
-            return f"{int(time / 3600)}:{round(time % 3600 / 60)} remaining"
+            return f"{int(time / 3600):0>2}:{round(time % 3600 / 60):0>2} remaining"
         if time > DISPLAY_SECONDS:
             return f"{int(time / 60)} minutes remaining"
         if time >= 60:
-            return f"{int(time / 60)}′ {round(time % 60)}″ remaining"
+            return f"{int(time / 60)}′ {round(time % 60):0>2}″ remaining"
         return f"{round(time)} seconds remaining"
 
 
@@ -316,7 +242,7 @@ async def on_message(message):
         prefix += f":{message.channel.name}"
     else:
         prefix = f"{message.author.name}"
-    logger.info(f"[{prefix}] {content}")
+    logger.info(f"[{prefix}] Received: {content}")
     # timer already running in channel
     if message.channel in client.TIMERS:
         timer = client.TIMERS[message.channel]
@@ -325,6 +251,7 @@ async def on_message(message):
                 await timer.stop()
             elif content.lower() == "resume":
                 await timer.refresh()
+                logger.info(f"[{prefix}] Refreshed and resumed")
             else:
                 await message.channel.send(
                     embed=discord.Embed(
@@ -347,23 +274,24 @@ async def on_message(message):
                 )
             else:
                 await timer.refresh()
+                logger.info(f"[{prefix}] Refreshed")
         return
     # no timer running in channel
     total_time = get_initial_time(content)
     if total_time:
-        start_time = client.loop.time()
-        timer = Timer(start_time, total_time, message.channel)
-        await timer.refresh()
+        timer = Timer(message.channel, client.loop.time(), total_time, prefix)
+        await timer.run()
+        logger.info(f"[{prefix}] Initial timer finished")
     # if parsing fails, display help
     else:
         await message.channel.send(
             embed=discord.Embed(
                 title="Usage",
                 description=(
-                    "- `timer 2h` start a 2 hours timer\n"
-                    "- `timer 2.5h` start a 2 hours 30 minutes timer\n"
-                    "- `timer 2:45` start a 2 hours 45 minutes timer\n"
-                    "- `timer 30mn` start a 30 minutes timer\n"
+                    "- `timer 2h` starts a 2 hours timer\n"
+                    "- `timer 2.5h` starts a 2 hours 30 minutes timer\n"
+                    "- `timer 2:45` starts a 2 hours 45 minutes timer\n"
+                    "- `timer 30mn` starts a 30 minutes timer\n"
                     "- `timer` displays the current timer if there is one\n"
                     "- `timer stop` stops the current timer if there is one\n"
                 ),
@@ -382,6 +310,8 @@ def get_initial_time(message):
         multiplier = MULTIPLIER[match.get("unit") or "hour"]
     except KeyError:
         return
+    if not match.get("unit") and match.get("separator") == "'":
+        multiplier = MULTIPLIER["minute"]
     time = int(match.get("high") or 2) * multiplier
     time += float(match.get("fraction") or 0) * multiplier
     if multiplier > 1:
